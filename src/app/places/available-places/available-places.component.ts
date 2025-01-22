cd51cced68d2e7d74e018baf36d61629d13746ce
Raul <jimenezraul1981@gmail.com>
2025-01-22 11:03:29 -0500

    add HttpClient support to fetch available places on component initialization

--- src/app/places/available-places/available-places.component.ts
+++ src/app/places/available-places/available-places.component.ts
@@ -1,4 +1,5 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, inject, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 import { Place } from '../place.model';
 import { PlacesComponent } from '../places.component';
@@ -11,6 +12,17 @@ import { PlacesContainerComponent } from '../places-container/places-container.c
   styleUrl: './available-places.component.css',
   imports: [PlacesComponent, PlacesContainerComponent],
 })
-export class AvailablePlacesComponent {
+export class AvailablePlacesComponent implements OnInit {
   places = signal<Place[] | undefined>(undefined);
+  private httpClient = inject(HttpClient)
+
+  ngOnInit() {
+    this.httpClient.get<{places: Place[]}>('http://localhost:3000/places')
+      .subscribe({
+        next: (data) => {
+          console.log('places', data.places);
+          this.places.set(data.places);
+        }
+      });
+  }
 }
